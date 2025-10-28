28477a6849cce59c0e54914ebce5b0c4868101d5
turbovadim <yolkin.vadim@gmail.com>
2025-10-28 17:27:45 +0300

    1.4.6 release. Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-    kotlin("jvm") version "2.1.20"
-    kotlin("plugin.serialization") version "2.1.20"
-    id("com.gradleup.shadow") version "8.3.6"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
+    id("com.gradleup.shadow") version "9.2.2"
     `maven-publish`
 }
 
 group = "org.endera"
-version = "1.4.5"
+version = "1.4.6"
 
 repositories {
     mavenCentral()
@@ -22,8 +22,8 @@ repositories {
 
 dependencies {
     // Minecraft APIs
-    val exposedVersion = "0.61.0"
-    val ktorVersion = "3.1.2"
+    val exposedVersion = "1.0.0-rc-2"
+    val ktorVersion = "3.3.1"
 
     compileOnly("net.kyori:adventure-text-minimessage:4.16.0")
 
@@ -40,17 +40,17 @@ dependencies {
     api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 
-    api("com.zaxxer:HikariCP:6.3.0")
+    api("com.zaxxer:HikariCP:7.0.2")
 
-    api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.1")
-    api("com.charleskorn.kaml:kaml:0.76.0")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.9.0")
+    api("com.charleskorn.kaml:kaml:0.102.0")
 
 
     // Database drivers
-    runtimeOnly("com.mysql:mysql-connector-j:9.2.0")
-    runtimeOnly("org.postgresql:postgresql:42.7.5")
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.5.3")
-    runtimeOnly("com.h2database:h2:2.3.232")
+    runtimeOnly("com.mysql:mysql-connector-j:9.4.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.8")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.5.6")
+    runtimeOnly("com.h2database:h2:2.4.240")
 }
 
 publishing {
--- src/main/kotlin/org/endera/enderalib/utils/async/dbQuery.kt
+++ src/main/kotlin/org/endera/enderalib/utils/async/dbQuery.kt
@@ -1,7 +1,7 @@
 package org.endera.enderalib.utils.async
 
 import kotlinx.coroutines.Dispatchers
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.v1.jdbc.transactions.experimental.newSuspendedTransaction
 
 @Suppress("unused")
 suspend fun <T> dbQuery(block: suspend () -> T): T =
